01f31f27fab7db62bf9b30f2ab438255a40b5cee
vikapitoshka <kapitonenko.vick@ya.ru>
2018-09-14 01:39:04 +0300

    Футер: соц. сети рендерятся через vue

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ var menu = new Vue({
 var wrapper = new Vue({
   el: '.wrapper'
 });
+
+var social = new Vue({
+  el: '.footer__social-list',
+  data() {
+    return {
+      socials: [
+        { name: 'snapchat', icon: 'fa-snapchat', link: '#' },
+        { name: 'telegram', icon: 'fa-telegram-plane', link: '#' },
+        { name: 'twitter', icon: 'fa-twitter', link: '#' },
+        { name: 'instagram', icon: 'fa-instagram', link: '#' },
+        { name: 'facebook', icon: 'fa-facebook-f', link: '#' }
+      ] 
+    };
+  }
+});
