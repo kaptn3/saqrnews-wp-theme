5720f4c160ec5c01724ef56a26a7aaeeba2d46da
Viktoriya <kapitonenko.vick@ya.ru>
2018-09-16 19:29:02 +0300

    Сайдбар: смена названий классов

--- functions.php
+++ functions.php
@@ -33,9 +33,9 @@ function saqrnews_widgets_init() {
     'name'          => esc_html__( 'Sidebar', 'saqrnews' ),
     'id'            => 'sidebar-1',
     'description'   => esc_html__( 'Add widgets here.', 'saqrnews' ),
-    'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    'before_widget' => '<section id="%1$s" class="sidebar__widget %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h2 class="widget-title">',
+    'before_title'  => '<h2 class="sidebar__widget-title">',
     'after_title'   => '</h2>',
   ) );
 }
