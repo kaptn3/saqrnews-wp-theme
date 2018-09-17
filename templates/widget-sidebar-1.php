14952da9a3744e9c5b871ec6ea6ddbfd535f86f7
Viktoriya <kapitonenko.vick@ya.ru>
2018-09-17 21:22:55 +0300

    Сайдбар: в первом блоке изображения - ссылки

--- templates/widget-sidebar-1.php
+++ templates/widget-sidebar-1.php
@@ -1,7 +1,7 @@
 <div class="sidebar__posts-card">
-    <div class="sidebar__posts-tumb">
+    <a class="sidebar__posts-tumb" href="<?php the_permalink() ?>">
         <?php the_post_thumbnail( $id, 'medium' ); ?>
-    </div>
+    </a>
     <div class="sidebar__posts-text">
         <h3 class="sidebar__posts-title"><a href="<?php the_permalink() ?>" class="posts__card-link"><?php the_title(); ?></a></h3>
         <span class="sidebar__posts-date"><?php the_time(get_option('date_format')) ?> <i class="fas fa-calendar"></i></span>
