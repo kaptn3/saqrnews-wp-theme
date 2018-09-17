40b0e8d9f2e6d7797a59bd4a06a3a06d35714189
Viktoriya <kapitonenko.vick@ya.ru>
2018-09-17 14:33:20 +0300

    Главныйй шаблон: заголовок, изображение, контент

--- index.php
+++ index.php
@@ -4,10 +4,20 @@
     <?php
     if ( have_posts() ) : 
         while ( have_posts() ) : the_post();
-        the_title();
+            if ( get_the_post_thumbnail() ) : 
+            ?>
+                <div class="article__image">
+                    <?php the_post_thumbnail( $id, 'full' ); ?>
+                </div>
+            <?php endif; ?>
+            <h2 class="article__title"><?php the_title(); ?></h2>
+            <div class="article__content">
+                <?php the_content(); ?>
+            </div>
+        <?php
         endwhile;
     else :
-        echo "Nothing Found";
+        echo "<h2 class='article__title'>Nothing Found</h2>";
     endif;
     ?>
 </div>
