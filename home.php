e740124feef415557bdd3d996f845c6941115a93
Viktoriya <kapitonenko.vick@ya.ru>
2018-09-26 11:22:14 +0300

    Главная: вывод опредённой рубрики

--- home.php
+++ home.php
@@ -19,35 +19,33 @@
   </section>
 
   <?php 
-  $category_id = 1;
-  if (get_cat_name($category_id)) {
-  ?>
+  $category = 'حياة-اجتماعية';
+  $args = array(
+    'category_name' => $category,
+    'posts_per_page' => 11,
+  );
+  $query = new WP_Query( $args );
+
+  if ( $query->have_posts() ) : ?>
     <section class="category-block">
       <header class="posts-list-title">
-        <h2><?php echo get_cat_name($category_id); ?></h2>
+        <h2><?php echo get_category_by_slug($category)->cat_name; ?></h2>
       </header>
-      <div class="category-posts">
-        <?php 
-        $args = array(
-          'category_in' => $category_id,
-          'orderby' => 'rand',
-          'posts_per_page' => 11,
-        );
-        $query = new WP_Query( $args );
-
-        if ( $query->have_posts() ) {
-          while ( $query->have_posts() ) {
-            $query->the_post();
-            if ( ($query->current_post === 6) || ($query->current_post === 0) || ($query->current_post === 1) ) {
-              get_template_part('templates/post-card'); # большие карточки в позициях 0, 1, 6 выводятся с количеством комментариев
-            } else {
-              get_template_part('templates/post-card-no-comments'); # без количетсва комментариев
-            }
+      <div class="category-posts">  
+        <?php
+        while ( $query->have_posts() ) :
+          $query->the_post();
+          if ( ($query->current_post === 6) || ($query->current_post === 0) || ($query->current_post === 1) ) {
+            get_template_part('templates/post-card'); # большие карточки в позициях 0, 1, 6 выводятся с количеством комментариев
+          } else {
+            get_template_part('templates/post-card-no-comments'); # без количетсва комментариев
           }
-        }
-        wp_reset_postdata(); ?>
+        endwhile; ?>
       </div>
     </section>
+    <?php    
+    endif;
+    wp_reset_postdata(); ?>
 
     <section class="last-news">
         <header class="posts-list-title">
@@ -66,9 +64,6 @@
         load_more_button();
         ?>
     </section>
-  <?php
-  }
-  ?>
 </div>
 
 <?php get_sidebar(); ?>
