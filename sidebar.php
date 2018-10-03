be7c8d2eaf77e44a05518f1d3a8fa6ca5141a13f
Viktoriya <kapitonenko.vick@ya.ru>
2018-10-03 15:47:54 +0300

    Сайдбар: наиболее просмотренные посты

--- sidebar.php
+++ sidebar.php
@@ -25,18 +25,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
       <div class="sidebar__top-posts">
         <h2 class="sidebar__widget-title sidebar__header-top-posts">أعلى المشاركات</h2>
         <?php 
-        $args = array(
-          'posts_per_page' => 2
-        );
-        $query = new WP_Query( $args );
+        $popularpost = new WP_Query( array( 'posts_per_page' => 4, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'DESC'  ) );
+        while ( $popularpost->have_posts() ) : $popularpost->the_post();
+        
+        get_template_part('templates/widget-sidebar-2');
+        
+        endwhile;
+        ?>
 
-        if ( $query->have_posts() ) {
-          while ( $query->have_posts() ) {
-            $query->the_post();
-            get_template_part('templates/widget-sidebar-2');
-          }
-        }
-        wp_reset_postdata(); ?>
       </div>
   <?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
