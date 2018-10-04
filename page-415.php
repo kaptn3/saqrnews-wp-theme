782f9a28b3a819b5df5734dab8403b717f566270
Viktoriya <kapitonenko.vick@ya.ru>
2018-10-04 18:34:01 +0300

    Гороскоп: информация по периоду и зодиаку

--- page-415.php
+++ page-415.php
@@ -1,7 +1,27 @@
 <?php
-    $url = file_get_contents('http://www.elabraj.net/ar/horoscope/daily/8');
-    $div = '#\<div class="horoscope-daily-text"\>(.+?)\<\/div\>#s';
-    preg_match_all($div, $url, $resul);
-    #print_r($resul);
-    var_dump($resul);
-?>
+    if ( ( isset( $_GET['zodiac'] ) ) && ( isset( $_GET['zodiac'] ) ) ) {
+        $zodiac = $_GET['zodiac'];
+        $period = $_GET['period'];
+        $site = 'http://www.elabraj.net/ar/horoscope/';
+        $url = $site . $period . '/' . $zodiac;
+        $content = file_get_contents( $url );
+
+        if ( $period === 'daily') {
+            $first_step = explode( '<div class="horoscope-daily-text">' , $content );
+            $second_step = explode( "</div>" , $first_step[4] );
+        } else {
+            $first_step = explode( '<div class="horoscope-content-body">', $content );
+            $second_step = explode( "</div>" , $first_step[1] );
+        }
+        
+        print_r( $second_step[0] );
+    } else {
+    ?>
+    <div class="horoscopes">
+        <?php 
+        for ($i = 1; $i < 12; $i++) { 
+            echo '<a href="' . '?period=daily&zodiac=' . $i . '">Text</a>';  
+        }
+        ?>
+    </div>
+<?php } ?>
