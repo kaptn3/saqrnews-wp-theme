72adb7e2e7f99dce1aa9d534a1873146617db3d0
Viktoriya <kapitonenko.vick@ya.ru>
2018-10-14 15:04:28 +0300

    Кнопка "загрузить" для других страниц вывода постов

--- category.php
+++ category.php
@@ -3,12 +3,18 @@
 <div class="wrapper">
     <?php
     if ( have_posts() ) : 
+    ?>
+    <div id="ajax">
+        <?php
         while ( have_posts() ) : the_post();
             get_template_part('templates/post-list-card');
-        endwhile;
+        endwhile; ?>
+    </div>
+    <?php
     else :
         echo "<h2 class='article__title'>Nothing Found</h2>";
     endif;
+    load_more_button();
     ?>
 </div>
 
--- search.php
+++ search.php
@@ -2,10 +2,14 @@
 
 <div class="wrapper">
     <?php
-    if ( have_posts() ) : 
+    if ( have_posts() ) : ?>
+    <div id="ajax">
+        <?php
         while ( have_posts() ) : the_post();
             get_template_part('templates/post-list-card');
-        endwhile;
+        endwhile; ?>
+    </div>
+    <?php
     else :
         echo "<h2 class='article__title'>Nothing Found</h2>";
     endif;
